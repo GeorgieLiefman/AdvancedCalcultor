4d445b27c1c6c7acd4a086119bc7923240451bcf
GeorgieLiefman <gkb.liefman@gmail.com>
2022-08-22 10:13:32 +1000

    Cleaned up code

--- src/eval_calculator.pl
+++ src/eval_calculator.pl
@@ -2,11 +2,17 @@
 use strict;
 use warnings;
 
-while (defined(my $s = <>)) {
-    my $result = eval $s; 
-    if ($@) {  
-        print "Invalid string:\n $s"; 
-    } else { 
-        print $result, "\n"; 
-    } 
-} 
+sub main {
+    print("Enter your equation below.\n");
+    while (defined(my $x = <>)) {
+        my $result = eval $x; 
+        if ($@) {  
+            print "Invalid input: $x"; 
+            print "Please enter something else.\n"; 
+        } else { 
+            print $result, "\n"; 
+        } 
+    }
+}
+
+main() 
